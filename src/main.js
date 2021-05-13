63caf48de03e0d509d3fad2e817fa1dc9a7535e9
Sambhu <sambhubabu69@gmail.com>
2021-05-13 17:51:28 +0530

    -- Disable dark theme

--- src/main.js
+++ src/main.js
@@ -6,8 +6,9 @@ import store from './store'
 
 import VueMaterial from 'vue-material'
 import 'vue-material/dist/vue-material.min.css'
-// import 'vue-material/dist/theme/default.css'
-import 'vue-material/dist/theme/default-dark.css'
+import 'vue-material/dist/theme/default.css'
+// For Default Dark theme 
+// import 'vue-material/dist/theme/default-dark.css'
 
 Vue.use(VueMaterial)
 Vue.config.productionTip = false
